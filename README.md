af6fefe85d38777ce3f4e4443dff5e1ebeb4db73
Slacked Lime <slackedlime@proton.me>
2022-09-16 08:42:15 +0400

    Fixed Context Menu going out of window by adding scroll

--- README.md
+++ README.md
@@ -5,14 +5,16 @@
 **A simple GUI rust application that keeps track of how much time you spend on each application.**
 
 ## Usage
+
 The Daemon binary tracks the amount of time you used an application. Make sure it's running in the background.
 
 In the Gui, You can right click the names of applications to open a Context menu.
 
 ## Building from Source
+
 Install [Rust](https://www.rust-lang.org/tools/install) if you don't have it already
 
-```
+``` bash
 git clone https://github.com/slackedlime/TimeSpent.git
 cd TimeSpent/
 cargo build --bin daemon --release
@@ -24,9 +26,9 @@ You should find you executables in the /target/release/ directory
 Be sure to make the daemon binary a start up application.
 
 ## TODO
+
 - Upload to AUR.
 - Make Appimage.
 - Make an Installer for Windows.
 - Add Groups Feature.
 - Add a bargraph to Status menu.
-- Fix Context Menu going out of screen.
--- src/gui/contextmenu.rs
+++ src/gui/contextmenu.rs
@@ -22,61 +22,64 @@ macro_rules! open_window {
 impl TimeSpent {
 	pub fn draw_context_menu(&mut self, name: String, 
 		data: &serde_json::Value, ui: &mut eframe::egui::Ui) {
-
-		ui.vertical_centered(|ui| {
-			ui.monospace(name);
+		
+		eframe::egui::ScrollArea::vertical().show(ui, |ui| {
+			ui.vertical_centered(|ui| {
+				ui.monospace(name);
+			});
+	
+			ui.add_space(3.);
+	
+			if ui.button("More Info").clicked() {
+				open_window!(self.win.status_window, self.win.status_data, data);
+				ui.close_menu();
+			}
+	
+			if ui.button("Rename").clicked() {
+				open_window!(self.win.rename_window, self.win.rename_data, data);
+				ui.close_menu();
+			}
+			
+			let hide_button_text = 
+				if self.hidden_processes.contains(&json!(data["name"])) { 
+					"Unhide" 
+				} else {
+					 "Hide" 
+				};
+			
+			if ui.button(hide_button_text).clicked() {
+				
+				if hide_button_text == "Hide" {
+					self.hidden_processes.push( json!(data["name"]) );
+				} else {
+					// Remove that process from the list of hidden processes
+					self.hidden_processes.retain( |x| x != &data["name"] )
+				}
+				
+				match std::fs::write(&self.hidden_processes_file,
+					  json!(self.hidden_processes).to_string().as_bytes()) {
+	
+					Ok(_) => {},
+					Err(_) => println!("hidden.json Couldn't be written to disk"),
+				}
+	
+				ui.close_menu();
+			}
+	
+			if ui.button("Delete").clicked() {
+				open_window!(self.win.delete_window, self.win.delete_data, data);
+				ui.close_menu();
+			}
+	
+			if ui.button("Show Raw Data").clicked() {
+				open_window!(self.win.raw_data_window, self.win.raw_data, data);
+				ui.close_menu();
+			}
+	
+			if ui.button("Close the Menu").clicked() {
+				ui.close_menu();
+			}
 		});
 
-		ui.add_space(3.);
-
-		if ui.button("More Info").clicked() {
-			open_window!(self.win.status_window, self.win.status_data, data);
-			ui.close_menu();
-		}
-
-		if ui.button("Rename").clicked() {
-			open_window!(self.win.rename_window, self.win.rename_data, data);
-			ui.close_menu();
-		}
-		
-		let hide_button_text = 
-			if self.hidden_processes.contains(&json!(data["name"])) { 
-				"Unhide" 
-			} else {
-				 "Hide" 
-			};
-		
-		if ui.button(hide_button_text).clicked() {
-			
-			if hide_button_text == "Hide" {
-				self.hidden_processes.push( json!(data["name"]) );
-			} else {
-				// Remove that process from the list of hidden processes
-				self.hidden_processes.retain( |x| x != &data["name"] )
-			}
-			
-			match std::fs::write(&self.hidden_processes_file,
-				  json!(self.hidden_processes).to_string().as_bytes()) {
-
-				Ok(_) => {},
-				Err(_) => println!("hidden.json Couldn't be written to disk"),
-			}
-
-			ui.close_menu();
-		}
-
-		if ui.button("Delete").clicked() {
-			open_window!(self.win.delete_window, self.win.delete_data, data);
-			ui.close_menu();
-		}
-
-		if ui.button("Show Raw Data").clicked() {
-			open_window!(self.win.raw_data_window, self.win.raw_data, data);
-			ui.close_menu();
-		}
-
-		if ui.button("Close the Menu").clicked() {
-			ui.close_menu();
-		}
 	}
 }
