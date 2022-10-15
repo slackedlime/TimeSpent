fbda41c1fc18bbf9cad2066612105f3342175917
Slacked Lime <slackedlime@proton.me>
2022-10-15 13:31:10 +0400

    Icon only gets attached in release builds

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main() {
-	if cfg!(target_os = "windows") {
+	if cfg!(target_os = "windows") && !cfg!(debug_assertions) {
 		let mut res = winres::WindowsResource::new();
 		res.set_icon("imgs/hummingbird_new.ico");
 
