2899bad1c164d26ebdc7e298ce77841e1d564ed0
Slacked Lime <slackedlime@proton.me>
2022-10-15 13:24:53 +0400

    Icon now gets automatically attached at compile time

---
+++ build.rs
@@ -0,0 +1,11 @@
+fn main() {
+	if cfg!(target_os = "windows") {
+		let mut res = winres::WindowsResource::new();
+		res.set_icon("imgs/hummingbird_new.ico");
+
+		if let Err(e) = res.compile() {
+			eprintln!("Could not attach Icon to executable");
+			eprintln!("Error: {}\n", e);
+		}
+	}
+}
